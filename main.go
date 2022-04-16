d1f3e1d38b2e5c550c5a51a28e97adef48100f32
batur <batur@batur.batur>
2022-04-16 15:50:27 +0300

    first commit

--- main.go
+++ main.go
@@ -1,25 +1,7 @@
 package main
 
-import "fmt"
+import "goTutorial/helloWorld"
 
 func main() {
-
-	var name string
-	var age int8
-	var isStudent bool
-
-	name = "Bob"
-	age = 27
-	isStudent = true
-
-	fmt.Println("Hello", name)
-	fmt.Println("you are", age, "years old.")
-	fmt.Println("you are", studentDecider(isStudent))
-}
-
-func studentDecider(isStudent bool) string {
-	if isStudent {
-		return "a student."
-	}
-	return "not a student."
+	helloWorld.HelloWorld()
 }
