c00cac0080ede060551fedef5315a6aa916e6560
batur <batur@batur.batur>
2022-04-17 16:43:15 +0300

    arrays

--- main.go
+++ main.go
@@ -1,15 +1,21 @@
 package main
 
 import (
+	"goTutorial/arithmeticOperations"
+	"goTutorial/arrays"
+	"goTutorial/helloWorld"
+	"goTutorial/loops"
 	"goTutorial/slices"
+	"goTutorial/statementsAndConditions"
+	"goTutorial/variables"
 )
 
 func main() {
-	//helloWorld.HelloWorld()
-	//arithmeticOperations.ArithmeticOperations()
-	//variables.Variables()
-	//loops.Loops()
-	//statementsAndConditions.StatementsAndConditions()
-	//arrays.Arrays()
+	helloWorld.HelloWorld()
+	arithmeticOperations.ArithmeticOperations()
+	variables.Variables()
+	loops.Loops()
+	statementsAndConditions.StatementsAndConditions()
+	arrays.Arrays()
 	slices.Slices()
 }
