4a53bd226e291a8e679d1a6b3daedd5c2c326b16
batur <batur@batur.batur>
2022-04-17 15:06:16 +0300

    if/else statements

--- main.go
+++ main.go
@@ -1,13 +1,17 @@
 package main
 
 import (
+	"goTutorial/arithmeticOperations"
+	"goTutorial/helloWorld"
+	"goTutorial/loops"
 	"goTutorial/statementsAndConditions"
+	"goTutorial/variables"
 )
 
 func main() {
-	//helloWorld.HelloWorld()
-	//arithmeticOperations.ArithmeticOperations()
-	//variables.Variables()
-	//loops.Loops()
+	helloWorld.HelloWorld()
+	arithmeticOperations.ArithmeticOperations()
+	variables.Variables()
+	loops.Loops()
 	statementsAndConditions.StatementsAndConditions()
 }
