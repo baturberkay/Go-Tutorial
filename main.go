8cb5f11540344670d99011a9efec44cdcc5f791e
baturberkay <baturberkaydev@gmail.com>
2022-07-10 18:36:46 +0300

    Functions added

--- main.go
+++ main.go
@@ -1,23 +1,17 @@
 package main
 
 import (
-	"goTutorial/arithmeticOperations"
-	"goTutorial/arrays"
-	"goTutorial/helloWorld"
-	"goTutorial/loops"
-	"goTutorial/maps"
-	"goTutorial/slices"
-	"goTutorial/statementsAndConditions"
-	"goTutorial/variables"
+	"goTutorial/functions"
 )
 
 func main() {
-	helloWorld.HelloWorld()
-	arithmeticOperations.ArithmeticOperations()
-	variables.Variables()
-	loops.Loops()
-	statementsAndConditions.StatementsAndConditions()
-	arrays.Arrays()
-	slices.Slices()
-	maps.Maps()
+	//helloWorld.HelloWorld()
+	//arithmeticOperations.ArithmeticOperations()
+	//variables.Variables()
+	//loops.Loops()
+	//statementsAndConditions.StatementsAndConditions()
+	//arrays.Arrays()
+	//slices.Slices()
+	//maps.Maps()
+	functions.Functions()
 }
